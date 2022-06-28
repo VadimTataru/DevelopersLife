3812eee61ab1da66ee4cbb16793e4f62130b3fd1
VadimTataru <64976256+VadimTataru@users.noreply.github.com>
2022-06-28 19:41:26 +0300

    Trying to add navigation

--- app/src/main/java/com/example/developerslife/presentation/MainActivity.kt
+++ app/src/main/java/com/example/developerslife/presentation/MainActivity.kt
@@ -2,23 +2,34 @@ package com.example.developerslife.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentActivity
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.NavHostFragment
 import com.example.developerslife.R
 import com.example.developerslife.databinding.ActivityMainBinding
 import com.example.developerslife.presentation.fragments.mainscreen.MainScreenFragment
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : FragmentActivity() {
     lateinit var binding: ActivityMainBinding
+    lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        //initNavController()
 
-        supportFragmentManager.beginTransaction()
+        /*supportFragmentManager.beginTransaction()
             .replace(
                 R.id.flContainer,
                 MainScreenFragment()
             )
-            .commit()
+            .commit()*/
+    }
+
+    private fun initNavController() {
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
+        navController = navHostFragment.navController
     }
 }
